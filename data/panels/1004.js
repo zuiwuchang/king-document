03934b69bcbc6f91e00d6153b1973dedde25fb30
zuiwuchang <zuiwuchang@gmail.com>
2017-07-12 13:13:49 +0800

    add Poco::Data::Sqlite auto id

--- data/panels/1004.js
+++ data/panels/1004.js
@@ -3,56 +3,63 @@ var __v=[
 			"Id": 1916,
 			"Panel": 1004,
 			"Name": "SQLite",
-			"Sort": 0,
+			"Sort": 9,
+			"Str": ""
+		},
+		{
+			"Id": 3793,
+			"Panel": 1004,
+			"Name": "自增id",
+			"Sort": 8,
 			"Str": ""
 		},
 		{
 			"Id": 1917,
 			"Panel": 1004,
 			"Name": "Example",
-			"Sort": 0,
+			"Sort": 7,
 			"Str": ""
 		},
 		{
 			"Id": 1918,
 			"Panel": 1004,
 			"Name": "Example binary",
-			"Sort": 0,
+			"Sort": 6,
 			"Str": ""
 		},
 		{
 			"Id": 1919,
 			"Panel": 1004,
 			"Name": "映射自定義型別",
-			"Sort": 0,
+			"Sort": 5,
 			"Str": ""
 		},
 		{
 			"Id": 1920,
 			"Panel": 1004,
 			"Name": "Example",
-			"Sort": 0,
+			"Sort": 4,
 			"Str": ""
 		},
 		{
 			"Id": 1921,
 			"Panel": 1004,
 			"Name": "SQLite 加密",
-			"Sort": 0,
+			"Sort": 3,
 			"Str": ""
 		},
 		{
 			"Id": 1922,
 			"Panel": 1004,
 			"Name": "Example",
-			"Sort": 0,
+			"Sort": 2,
 			"Str": ""
 		},
 		{
 			"Id": 1923,
 			"Panel": 1004,
 			"Name": "注意",
-			"Sort": 0,
+			"Sort": 1,
 			"Str": ""
 		}
 	]
