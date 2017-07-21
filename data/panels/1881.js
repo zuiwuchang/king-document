67ac2833cf29c6fe79191c3ff08875e30abbac97
zuiwuchang <zuiwuchang@gmail.com>
2017-07-21 15:12:30 +0800

    add python list

--- data/panels/1881.js
+++ data/panels/1881.js
@@ -1,1 +1,44 @@
-var __v=null
+var __v=[
+		{
+			"Id": 3817,
+			"Panel": 1881,
+			"Name": "list",
+			"Sort": 0,
+			"Str": ""
+		},
+		{
+			"Id": 3818,
+			"Panel": 1881,
+			"Name": "列表方法",
+			"Sort": 0,
+			"Str": ""
+		},
+		{
+			"Id": 3819,
+			"Panel": 1881,
+			"Name": "拆分",
+			"Sort": 0,
+			"Str": ""
+		},
+		{
+			"Id": 3820,
+			"Panel": 1881,
+			"Name": "*參數",
+			"Sort": 0,
+			"Str": ""
+		},
+		{
+			"Id": 3821,
+			"Panel": 1881,
+			"Name": "range",
+			"Sort": 0,
+			"Str": ""
+		},
+		{
+			"Id": 3822,
+			"Panel": 1881,
+			"Name": "分片",
+			"Sort": 0,
+			"Str": ""
+		}
+	]
