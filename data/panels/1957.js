16a2ce23ccfbb4d7c91b4184c4829b67e0782fe2
zuiwuchang <zuiwuchang@gmail.com>
2017-09-01 16:38:45 +0800

    add cocos creator event

---
+++ data/panels/1957.js
@@ -0,0 +1,23 @@
+var __v=[
+		{
+			"Id": 4001,
+			"Panel": 1957,
+			"Name": "emit 發送事件",
+			"Sort": 0,
+			"Str": ""
+		},
+		{
+			"Id": 4002,
+			"Panel": 1957,
+			"Name": "dispatchEvent 派送事件",
+			"Sort": 0,
+			"Str": ""
+		},
+		{
+			"Id": 4003,
+			"Panel": 1957,
+			"Name": "Event",
+			"Sort": 0,
+			"Str": ""
+		}
+	]
