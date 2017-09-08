947151527016a4334b0e4c3f3c8fb8bbbb656f00
zuiwuchang <zuiwuchang@gmail.com>
2017-09-08 14:42:45 +0800

    add cocos Physics

--- data/chapters/614.js
+++ data/chapters/614.js
@@ -10,5 +10,35 @@ var __v=[
 			"Chapter": 614,
 			"Name": "查詢物體",
 			"Sort": 0
+		},
+		{
+			"Id": 1996,
+			"Chapter": 614,
+			"Name": "剛體",
+			"Sort": 0
+		},
+		{
+			"Id": 1997,
+			"Chapter": 614,
+			"Name": "剛體方法",
+			"Sort": 0
+		},
+		{
+			"Id": 1998,
+			"Chapter": 614,
+			"Name": "物理碰撞組件",
+			"Sort": 0
+		},
+		{
+			"Id": 1999,
+			"Chapter": 614,
+			"Name": "碰撞回調",
+			"Sort": 0
+		},
+		{
+			"Id": 2000,
+			"Chapter": 614,
+			"Name": "關節組件",
+			"Sort": 0
 		}
 	]
