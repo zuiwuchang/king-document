61f7aeadb8df0f0258a25739380e217f4118fdee
zuiwuchang <zuiwuchang@gmail.com>
2017-10-23 16:11:44 +0800

    add vs2015 codeblock pch

--- data/panels/2047.js
+++ data/panels/2047.js
@@ -3,14 +3,21 @@ var __v=[
 			"Id": 4217,
 			"Panel": 2047,
 			"Name": "codeblock",
-			"Sort": 0,
+			"Sort": 3,
+			"Str": ""
+		},
+		{
+			"Id": 4219,
+			"Panel": 2047,
+			"Name": "預編譯",
+			"Sort": 2,
 			"Str": ""
 		},
 		{
 			"Id": 4218,
 			"Panel": 2047,
 			"Name": "注意",
-			"Sort": 0,
+			"Sort": 1,
 			"Str": ""
 		}
 	]
